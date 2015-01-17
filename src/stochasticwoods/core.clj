c04fd8ab3e899e3078bfa94de8bee79db7be2e7e
KC Anderson <kelsey.anderson@ucdenver.edu>
2015-01-16 23:39:39 -0700

    More musings.

--- src/stochasticwoods/core.clj
+++ src/stochasticwoods/core.clj
@@ -29,16 +29,54 @@
                    {% :numeric})
                 ks))))
 
+(defn infer-possible-vals
+  ([data] (infer-possible-vals data
+                               (if (map? (first data))
+                                 (keys (first data))
+                                 (range (count (first data))))))
+  ([data ks]
+   (let [f (fn [k] (apply hash-set (map #(get % k) data)))]
+     (apply merge (map (fn [k] {k (f k)}) ks)))))
+
 (defn- break-fns
-  ([data] (break-fns data (infer-datatypes data)))
-  ([data datatypes]
-   (fn [n]
-     (let [b (apply hash-set (map #(get % n) data))]
+  ([data ks] (break-fns data ks (infer-datatypes data)))
+  ([data ks datatypes]
+   (let [datasets (infer-possible-vals data ks)]
+     (fn [n]
        (map (fn [p]
               (if (= :categorical (datatypes n))
                 [(fn [x] (= (get x n) p)) (str "[" n "] == " p)]
                 [(fn [x] (<= (get x n) p)) (str "[" n "] <= " p)]))
-            b)))))
+            (datasets n))))))
+
+;; (defn- partition-data-by-breakfn
+;;   "This ugly function sorts the data into
+;;   two bins using the break function."
+;;   [data response]
+;;   (let [m (count data)]
+;;     (fn [[bfn s]]
+;;       (loop [leftd (list)
+;;              rightd (list)
+;;              leftr (list)
+;;              rightr (list)
+;;              i 0]
+;;         (if (< i m)
+;;           (if (bfn (nth data i))
+;;             (recur (conj leftd (nth data i))
+;;                    rightd
+;;                    (conj leftr (nth response i))
+;;                    rightr
+;;                    (inc i))
+;;             (recur leftd
+;;                    (conj rightd (nth data i))
+;;                    leftr
+;;                    (conj rightr (nth response i))
+;;                    (inc i)))
+;;           [[leftd leftr] [rightd rightr]])))))
+
+(defn- special-assoc!
+  [m k v]
+  (assoc! m k (inc (get m k 0))))
 
 (defn- partition-data-by-breakfn
   "This ugly function sorts the data into
@@ -46,62 +84,88 @@
   [data response]
   (let [m (count data)]
     (fn [[bfn s]]
-      (loop [leftd []
-             rightd []
-             leftr []
-             rightr []
+      (loop [leftd (list)
+             rightd (list)
+             leftr (list)
+             rightr (list)
+             lvals (transient {})
+             rvals (transient {})
              i 0]
         (if (< i m)
-          (if (bfn (nth data i))
-            (recur (conj leftd (nth data i))
-                   rightd
-                   (conj leftr (nth response i))
-                   rightr
-                   (inc i))
-            (recur leftd
-                   (conj rightd (nth data i))
-                   leftr
-                   (conj rightr (nth response i))
-                   (inc i)))
-          [[leftd leftr] [rightd rightr]])))))
+          (let [d (nth data i)
+                r (nth response i)]
+            (if (bfn d)
+              (recur (conj leftd d)
+                     rightd
+                     (conj leftr r)
+                     rightr
+                     (special-assoc! lvals r 1)
+                     rvals
+                     (inc i))
+              (recur leftd
+                     (conj rightd d)
+                     leftr
+                     (conj rightr r)
+                     lvals
+                     (special-assoc! rvals r 1)
+                     (inc i))))
+          [[leftd leftr (persistent! lvals)]
+           [rightd rightr (persistent! rvals)]])))))
+
+;; (defn gini-impurity-after-break
+;;   [[[ld lr] [rd rr]]]
+;;   ;;(* 0.5 (+  (gini-impurity lr) (gini-impurity rr)))
+;;   ;;(max (gini-impurity lr) (gini-impurity rr))
+;;   (let [n (+ (count lr) (count rr))]
+;;     (+ (* (gini-impurity lr) (/ (count lr) n))
+;;        (* (gini-impurity rr) (/ (count rr) n)))))
+
+(defn- gifromfreqs
+  [f]
+  (let [n (reduce + (map second f))]
+    (- 1 (reduce + (map (fn [[k v]] (square (/ v n))) f)))))
 
 (defn gini-impurity-after-break
-  [[[ld lr] [rd rr]]]
+  [[[ld lr lv] [rd rr rv]]]
   ;;(* 0.5 (+  (gini-impurity lr) (gini-impurity rr)))
   ;;(max (gini-impurity lr) (gini-impurity rr))
   (let [n (+ (count lr) (count rr))]
-    (+ (* (gini-impurity lr) (/ (count lr) n))
-       (* (gini-impurity rr) (/ (count rr) n)))))
+    (+ (* (gifromfreqs lv)
+          (/ (count lr) n))
+       (* (gifromfreqs rv)
+          (/ (count rr) n)))))
 
 (defn grab-details
   [data response breakfns]
   (map #(let [partition_fn (partition-data-by-breakfn data response)
               p (partition_fn %)]
-          (vector % p (gini-impurity-after-break p)))
+          (list % p (gini-impurity-after-break p)))
        breakfns))
 
 (defn build-decision-tree
-  [data response & {:keys [datatypes columnkeys]
-                    :or {datatypes (infer-datatypes data)
-                         columnkeys (if (map? (first data))
-                                      (keys (first data))
-                                      (range (count (first data))))}}]
-  (let [gi0 (gini-impurity response)
-        m (count data)
-        n (count columnkeys)
-        breakfns (mapcat (break-fns data datatypes) columnkeys)
-        allbreaks (grab-details data response breakfns)
-        [mf [[lpd lpr] [rpd rpr]] gi] (reduce (partial min-key last)
-                                              allbreaks)]
-    (cond
-      (<= gi0 gi) (frequencies response)
-      :else (list mf
-                  (build-decision-tree lpd lpr
-                                       :dataypes datatypes
-                                       :columnkeys columnkeys)
-                  (build-decision-tree rpd rpr
-                                       :dataypes datatypes
-                                       :columnkeys columnkeys)))))
+  ([data response]
+   (build-decision-tree data
+                        response
+                        (if (map? (first data))
+                          (keys (first data))
+                          (range (count (first data))))
+                        (infer-datatypes data)))
+  ([data response columnkeys datatypes]
+   (let [gi0 (gini-impurity response)
+         m (count data)
+         n (count columnkeys)
+         foo (break-fns data columnkeys datatypes)
+         breakfns (mapcat foo columnkeys)
+         allbreaks (grab-details data response breakfns)
+         [mf [[lpd lpr] [rpd rpr]] gi] (reduce (partial min-key last)
+                                               allbreaks)]
+     (cond
+       (<= gi0 gi) (frequencies response)
+       :else (list mf
+                   (build-decision-tree (into [] lpd) (into [] lpr)
+                                        columnkeys datatypes)
+                   (build-decision-tree (into [] rpd) (into [] rpr)
+                                        columnkeys datatypes))))))
 
 ;; Printing trees
 (defn adjacency-list
@@ -203,11 +267,12 @@
   [data response]
   (let [ks (vec (if (map? (first data)) (keys (first data))
                     (range (count (first data)))))
-        ntrees 700
+        ntrees 500
+        datatypes (infer-datatypes data)
         indices (vec (range (count data)))
-        nsamples (int (/ (count data) 4))
+        nsamples (int (/ (count data) 1))
         nfeatures (min (count ks)
-                       (+ 5 (int (java.lang.Math/sqrt (count ks)))))
+                       (+ 2 (int (java.lang.Math/sqrt (count ks)))))
         samplefn (fn [] [(sample-with-replacement indices nsamples)
                         (sample-without-replacement ks nfeatures)])
         datafn (fn [sinds] (mapv (partial get data) sinds))
@@ -215,10 +280,11 @@
         f (fn [[sind features]]
             (build-decision-tree (datafn sind)
                                  (responsefn sind)
-                                 :columnkeys features))]
+                                 features
+                                 datatypes))]
     (take ntrees
           (pmap #(hash-map :features (second %) :tree (f %))
-               (repeatedly samplefn)))))
+                (repeatedly samplefn)))))
 
 (defn train-and-validate
   [traindata trainresponse testdata testresponse]
@@ -229,62 +295,62 @@
                      :incorrect))]
     (frequencies (map f testdata testresponse))))
 
-(train-and-validate (into [] (take 300 data))
-                    (into [] (take 300 response))
-                    (into [] (nthrest data 300))
-                    (into [] (nthrest response 300)))
+(time (train-and-validate (into [] (take 300 data))
+                          (into [] (take 300 response))
+                          (into [] (nthrest data 300))
+                          (into [] (nthrest response 300))))
 
 
-;; (def forest (build-forest data response))
-;; (time (count forest))
-;; (predict-from-forest (map :tree forest) (last data))
-;; (last response)
-;; (def t (build-decision-tree data response))
-;; (classify-from-tree t [:female 100])
-;; (time (predict-from-forest (repeat 1000 t) [:male 50]))
+(def forest (build-forest data response))
+(time (count forest))
 
-;; (def blah (read-string (slurp "/home/kanderson/Downloads/test.edn")))
-;; (def data (map #(dissoc % :new_tumor_event_after_initial_treatment)
-;;                blah))
-;; (def response (map :new_tumor_event_after_initial_treatment blah))
 
-;; (def f "/home/kanderson/Code/Bioinformatics/lung_risk/missing_filled_with_year.csv")
-;; (def foo (with-open [rdr (clojure.java.io/reader f)]
-;;            (let [l (line-seq rdr)
-;;                  ks (map keyword
-;;                          (clojure.string/split (first l)
-;;                                                #","))
-;;                  f (fn [row] (map read-string
-;;                                  (clojure.string/split row #",")))
-;;                  f2 (fn [row] (zipmap ks (f row)))]
-;;              (mapv f2 (rest l)))))
-;; (def data (mapv #(dissoc % :new_tumor_event_after_initial_treatment)
-;;                 foo))
-;; (def response (mapv :new_tumor_event_after_initial_treatment foo))
-;; (time (build-decision-tree data response))
+(def t (build-decision-tree data response))
+(classify-from-tree t [:female 100])
+(time (predict-from-forest (repeat 1000 t) [:male 50]))
 
-;; (def data
-;;   (into []
-;;         (repeatedly 1000 #(if (> 0.5 (rand))
-;;                             [:male (rand-int 100)]
-;;                             [:female (rand-int 100)]))))
-;; (def response
-;;   (mapv (fn [[s a]] (if (= s :male)
-;;                      (if (> a 80)
-;;                        :yes
-;;                        (if (< (rand) 0.8)
-;;                          :no
-;;                          :yes))
-;;                      :no))
-;;         data))
+(def blah (read-string (slurp "/home/kanderson/Downloads/test.edn")))
+(def data (map #(dissoc % :new_tumor_event_after_initial_treatment)
+               blah))
+(def response (map :new_tumor_event_after_initial_treatment blah))
 
-;; (infer-datatypes data)
-;; (frequencies response)
-;; (time (build-decision-tree data response))
-;; (second (adjacency-list (build-decision-tree data response)))
-;; (spit "/home/kanderson/Downloads/foobar.dot"
-;;       (dot-format (build-decision-tree data response
-;;                                        :columnkeys [:tumor_weight])))
+(def f "/home/kanderson/Code/Bioinformatics/lung_risk/missing_filled_with_year.csv")
+(def foo (with-open [rdr (clojure.java.io/reader f)]
+           (let [l (line-seq rdr)
+                 ks (map keyword
+                         (clojure.string/split (first l)
+                                               #","))
+                 f (fn [row] (map read-string
+                                 (clojure.string/split row #",")))
+                 f2 (fn [row] (zipmap ks (f row)))]
+             (mapv f2 (rest l)))))
+(def data (mapv #(dissoc % :new_tumor_event_after_initial_treatment)
+                foo))
+(def response (mapv :new_tumor_event_after_initial_treatment foo))
+(time (build-decision-tree data response))
+
+(def data
+  (into []
+        (repeatedly 3000 #(if (> 0.5 (rand))
+                            [:male (rand-int 100) (rand) (rand)]
+                            [:female (rand-int 100) (rand) (rand)]))))
+(def response
+  (mapv (fn [[s a]] (if (= s :male)
+                     (if (> a 80)
+                       :yes
+                       (if (< (rand) 0.8)
+                         :no
+                         :yes))
+                     :no))
+        data))
+
+(infer-datatypes data)
+(frequencies response)
+(time (build-decision-tree data response))
+(second (adjacency-list (build-decision-tree data response)))
+(spit "/home/kanderson/Downloads/foobar.dot"
+      (dot-format (build-decision-tree data response
+                                       :columnkeys [:tumor_weight])))
 
 
 
